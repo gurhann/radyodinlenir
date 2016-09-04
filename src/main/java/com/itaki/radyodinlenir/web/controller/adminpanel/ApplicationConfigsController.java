fa4b5eeedc8588045b2762c235393f1f2fe1a15a
dursuncimen <dursun.gsate@gmail.com>
2016-09-04 23:09:52 +0300

    Insert Seo-Social-About Config Controller metot

--- src/main/java/com/itaki/radyodinlenir/web/controller/adminpanel/ApplicationConfigsController.java
+++ src/main/java/com/itaki/radyodinlenir/web/controller/adminpanel/ApplicationConfigsController.java
@@ -3,7 +3,6 @@ package com.itaki.radyodinlenir.web.controller.adminpanel;
 import java.util.List;
 import java.util.Locale;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
@@ -25,7 +24,6 @@ import com.itaki.radyodinlenir.web.validation.GeneralConfigFormValidator;
 
 @Controller
 public class ApplicationConfigsController {
-	private static final Logger logger = Logger.getLogger(ApplicationConfigsController.class);
 	@Autowired
 	ApplicationConfigService appConfigService;
 	@Autowired
@@ -39,39 +37,86 @@ public class ApplicationConfigsController {
 		binder.setValidator(generalConfigFormValidator);
 	}
 
-	@RequestMapping(value = "/admin/generalconfig", method = RequestMethod.GET)
+	@RequestMapping(value = "/admin/generalconfigs", method = RequestMethod.GET)
 	public String getGeneralConfigs(Model model, Locale locale) {
 		try {
-
-			List<ApplicationConfigDTO> appConfig = appConfigService.getApplicationConfigListBySize(0, 5);
-			GeneralConfigsFormDTO generalConfigsForm = new GeneralConfigsFormDTO();
-			generalConfigsForm.setConfigs(appConfig);
-			model.addAttribute("generalConfigsForm", generalConfigsForm);
-			return "generalconfigs";
+			return getData(model, 0, 4, "generalconfigs");
 		} catch (Exception e) {
 			return "error403";
 		}
 
 	}
 
-	@RequestMapping(value = "/admin/generalconfig", method = RequestMethod.POST)
+	@RequestMapping(value = "/admin/generalconfigs", method = RequestMethod.POST)
 	public String generalConfigSubmit(@ModelAttribute("generalConfigsForm") @Validated GeneralConfigsFormDTO generalConfigsForm, BindingResult result, Model model,
 			final RedirectAttributes redirectAttributes, Locale locale) {
+
+		return setData(generalConfigsForm, result, model, redirectAttributes, locale, "generalconfigs");
+
+	}
+
+	@RequestMapping(value = "/admin/seoconfigs", method = RequestMethod.GET)
+	public String getSeoConfigs(Model model, Locale locale) {
+		try {
+			return getData(model, 4, 6, "seoconfigs");
+		} catch (Exception e) {
+			return "error403";
+		}
+
+	}
+
+	@RequestMapping(value = "/admin/socialconfigs", method = RequestMethod.GET)
+	public String getSocialConfigs(Model model, Locale locale) {
+		try {
+			return getData(model, 10, 10, "socialconfigs");
+		} catch (Exception e) {
+			return "error403";
+		}
+
+	}
+
+	@RequestMapping(value = "/admin/aboutconfigs", method = RequestMethod.GET)
+	public String getAboutConfigs(Model model, Locale locale) {
+		try {
+			return getData(model, 20, 1, "aboutconfigs");
+		} catch (Exception e) {
+			return "error403";
+		}
+
+	}
+
+	@RequestMapping(value = "/admin/aboutconfigs", method = RequestMethod.POST)
+	public String aboutConfigSubmit(@ModelAttribute("generalConfigsForm") @Validated GeneralConfigsFormDTO generalConfigsForm, BindingResult result, Model model,
+			final RedirectAttributes redirectAttributes, Locale locale) {
+		return setData(generalConfigsForm, result, model, redirectAttributes, locale, "aboutconfigs");
+
+	}
+
+	public String getData(Model model, int firstIndex, int itemSize, String view) {
+		List<ApplicationConfigDTO> appConfig = appConfigService.getApplicationConfigListBySize(firstIndex, itemSize);
+		GeneralConfigsFormDTO generalConfigsForm = new GeneralConfigsFormDTO();
+		generalConfigsForm.setConfigs(appConfig);
+		model.addAttribute("generalConfigsForm", generalConfigsForm);
+		return view;
+	}
+
+	public String setData(GeneralConfigsFormDTO generalConfigsForm, BindingResult result, Model model, final RedirectAttributes redirectAttributes, Locale locale, String view) {
+
 		if (result.hasErrors()) {
 			model.addAttribute("css", "danger");
 			model.addAttribute("msg", msgsrc.getMessage("Form.Alert", new String[] {}, locale));
-			return "generalconfigs";
+			return "view";
 		}
 		try {
 			appConfigService.updateMultiApplicationConfig(generalConfigsForm.getConfigs());
 		} catch (ApplicationConfigNotFoundException e) {
 			model.addAttribute("css", "danger");
 			model.addAttribute("msg", msgsrc.getMessage("application.config.notfound", new String[] {}, locale));
-			return "generalconfigs";
+			return "view";
 		}
 		redirectAttributes.addFlashAttribute("css", "success");
 		redirectAttributes.addFlashAttribute("msg", msgsrc.getMessage("Form.Succesfull", new String[] {}, locale));
-		return "redirect:/admin/generalconfig";
+		return "redirect:/admin/" + view;
 
 	}
 
