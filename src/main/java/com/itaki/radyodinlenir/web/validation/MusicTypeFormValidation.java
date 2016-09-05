b461d1449e8a26395d39b13861e579100f50940b
dursuncimen <dursun.gsate@gmail.com>
2016-09-05 19:34:45 +0300

    Created Music Type Form Validation

---
+++ src/main/java/com/itaki/radyodinlenir/web/validation/MusicTypeFormValidation.java
@@ -0,0 +1,27 @@
+package com.itaki.radyodinlenir.web.validation;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import com.itaki.radyodinlenir.web.dto.MusicTypeDTO;
+
+@Component
+public class MusicTypeFormValidation implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return MusicTypeDTO.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		MusicTypeDTO musicType =(MusicTypeDTO) target;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty");
+        if(musicType.getName().length() > 255) {
+            errors.rejectValue("name", "MaxLength.255");
+        }
+	}
+
+}
