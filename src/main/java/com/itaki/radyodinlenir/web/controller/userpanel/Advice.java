4dc30249ccf7268c9abad3d04e595bce6061b88e
dursuncimen <dursun.gsate@gmail.com>
2016-09-21 07:47:36 +0300

    Added Left Bar for Cities

--- src/main/java/com/itaki/radyodinlenir/web/controller/userpanel/Advice.java
+++ src/main/java/com/itaki/radyodinlenir/web/controller/userpanel/Advice.java
@@ -6,18 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.itaki.radyodinlenir.service.ApplicationConfigService;
 import com.itaki.radyodinlenir.service.MusicTypeService;
+import com.itaki.radyodinlenir.service.impl.RadioStationCityServiceImpl;
 
 @ControllerAdvice
 public class Advice {
 
 	@Autowired
 	private MusicTypeService musicTypeService;
+	
+	@Autowired 
+	private ApplicationConfigService appConfigService;
+	
+	@Autowired
+	private RadioStationCityServiceImpl cityService;
 
 	@ModelAttribute
 	public void setMusicTypes(HttpServletRequest req) {
 		if (req.getSession().getAttribute("musicTypeList") == null) {
 			req.getSession().setAttribute("musicTypeList", musicTypeService.getAllMusicTypes());
+		}
+		if (req.getSession().getAttribute("configs") == null) {
+			req.getSession().setAttribute("configs", appConfigService.getAllApplicationConfigs());
+		}
+		if (req.getSession().getAttribute("cities") == null) {
+			req.getSession().setAttribute("cities", cityService.getAllRadioStationCity());
 		}
 	}
 }
---
+++ src/main/webapp/WEB-INF/layouts/userpanel/leftbar.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<div class="box info category">
+	<div class="box-heading">İllere Göre Radyolar</div>
+	<div class="box-content">
+		<div class="box-category">
+			<ul>				
+				<c:forEach var="city" items="${cities}">
+					<li><a href="/radiosofcity/${city.id}"><span>${city.id < 10 ? '0'.concat(city.id) : city.id}</span> ${city.name} </a></li>
+				</c:forEach>
+			</ul>
+		</div>
+	</div>
+</div>
