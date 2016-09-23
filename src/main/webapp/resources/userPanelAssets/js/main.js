c1d8ffad742d169d96ce75079f15692b57386ba2
dursuncimen <dursun.gsate@gmail.com>
2016-09-23 04:29:01 +0300

    Added pop-up sharing item for facebook

--- src/main/webapp/resources/userPanelAssets/js/main.js
+++ src/main/webapp/resources/userPanelAssets/js/main.js
@@ -128,6 +128,14 @@ jQuery(function($) {
         $this.prepend('<li class="gobackLvl"><a class="backLvl" href="#"><i class="fa fa-long-arrow-left"></i>Go Back</li>');
     });
 
-
+    $('.fb-share').click(function(e) {
+        e.preventDefault();
+        window.open($(this).attr('href'), 'fbShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+        return false;
+    });
 
 });
+
+
+
+
